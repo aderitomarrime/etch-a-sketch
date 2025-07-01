5bfe2f9194beab317866ef88afdade51ab2b202c
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-07-01 17:19:24 +0200

    feat: allow the user to create a new grid

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const numOfDivs = 256;
+const changeGridSizebtn = document.querySelector(".changebtn")
 
 function createGrid(num) {
     for (let i = 0; i < num; i++) {
@@ -18,3 +19,40 @@ function hoverEffect(item) {
         item.classList.add("hover-item")
     })
 }
+
+function allSquares () {
+    const squares = document.querySelectorAll(".square");
+    return squares;
+}
+
+function changeSquareSize (numOfSquares) {
+
+    let squareSize = 480 / numOfSquares;
+    let squares = allSquares()
+
+    squares.forEach((square) => {
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
+    })
+
+    squares.forEach(hoverEffect)
+}
+
+changeGridSizebtn.addEventListener("click", ()=> {
+    let newGridSize = window.prompt("Please enter the number of squares per side for the new grid");
+    if (newGridSize < 1 || newGridSize > 100) {
+        alert("Please enter a number between 1 and 100")
+    } else {
+        let numOfSquarePerSide = Number(newGridSize)
+        newGridSize = Number(newGridSize * newGridSize)
+
+        const squares = allSquares()
+        
+        squares.forEach((divsquare) => {
+            divsquare.remove();
+        })
+
+        createGrid(newGridSize);
+        changeSquareSize(numOfSquarePerSide);
+    }
+})
